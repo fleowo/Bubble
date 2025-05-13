d541e15c9a75fa8b9203a458442321003505cc0a
fleowo <wafflev2isme@gmail.com>
2025-05-14 01:05:35 +0700

    Loader

--- Loader.lua
+++ Loader.lua
@@ -1,23 +1,18 @@
 repeat task.wait() until game:IsLoaded() and game.Players.LocalPlayer
 repeat task.wait() until game.Players.LocalPlayer.Character
 
-local PromptLib = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/AlexR32/Roblox/refs/heads/main/Useful/PromptLibrary.lua"))()
 local MarketplaceService = game:GetService("MarketplaceService")
 local Players = game:GetService("Players")
 local VirtualUser = game:GetService("VirtualUser")
 
 if Bubble and Bubble.Loaded then
-    PromptLib("Error", "Bubble is already loaded \n(Error Code: 0)", {
-        {Text = "OK", LayoutOrder = 0, Primary = true, Callback = function() end}
-    })
+    print("Bubble is already loaded")
     return
 end
 
 local success, info = pcall(MarketplaceService.GetProductInfo, MarketplaceService, game.PlaceId)
 if not success then
-    PromptLib("Error", "Failed to retrieve product info. \n(Error Code: 0)", {
-        {Text = "OK", LayoutOrder = 0, Primary = true, Callback = function() end}
-    })
+    print("Failed to retrieve product info")
     return
 end
 
@@ -30,7 +25,6 @@ end
 
 getgenv().Bubble = {
     Loaded = false,
-    Utilities = {Misc = Loadscript("Utilities/Misc")},
     Games = {
         [6938803436]      = {Name = "Anime Dimensions."},
         [87039211657390]  = {Name = "Arise."}
@@ -42,18 +36,10 @@ for id, gameData in pairs(Bubble.Games) do
         print("Found supported game:", info.Name)
         Loadscript("Games/"..id)
         Bubble.Loaded = true
-
-        Players.LocalPlayer.Idled:connect(function()
-            VirtualUser:Button2Down(Vector2.new(0,0),Workspace.CurrentCamera.CFrame)
-            task.wait(1)
-            VirtualUser:Button2Up(Vector2.new(0,0),Workspace.CurrentCamera.CFrame)
-        end)
     end
 end
 
 if not Bubble.Loaded then
-    PromptLib("Error", "Bubble does not support this game \n(Error Code: 0)", {
-        {Text = "OK", LayoutOrder = 0, Primary = true, Callback = function() end}
-    })
+    print("Bubble does not support this game")
     return
 end
