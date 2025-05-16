d4fed2269c8a3c875ab9759e97ae11bbbf80b584
fleowo <wafflev2isme@gmail.com>
2025-05-16 19:04:45 +0700

    Loader - Added auto rejoin

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,12 @@
 repeat task.wait() until game:IsLoaded() and game.Players.LocalPlayer
 repeat task.wait() until game.Players.LocalPlayer.Character
 
-local HttpService = game:GetService("HttpService")
-local MarketplaceService = game:GetService("MarketplaceService")
 local Players = game:GetService("Players")
+local CoreGui = game:GetService("CoreGui")
 local VirtualUser = game:GetService("VirtualUser")
+local HttpService = game:GetService("HttpService")
+local TeleportService = game:GetService("TeleportService")
+local MarketplaceService = game:GetService("MarketplaceService")
 
 if Bubble and Bubble.Loaded then
     warn("Bubble is already loaded")
@@ -36,16 +38,17 @@ end
 getgenv().Bubble = {
     Loaded = false,
     Games = {
-        [2655311011] = {Name = "Anime Dimensions"},
-        [7074860883] = {Name = "Arise Crossover"},
+        [2655311011] = {Name = "Anime Dimensions", PlaceId = 6938803436},
+        [7074860883] = {Name = "Arise Crossover",  PlaceId = 87039211657390},
     }
 }
 
-for id, game in pairs(Bubble.Games) do
+for id, gameData in pairs(Bubble.Games) do
     if universeId == id then
-        print("Found supported game:", game.Name)
+        print("Found supported game:", gameData.Name)
         Loadscript("Games/"..id)
         Bubble.Loaded = true
+        PlaceId = gameData.PlaceId
 
         Players.LocalPlayer.Idled:connect(function()
             VirtualUser:CaptureController()
@@ -54,7 +57,14 @@ for id, game in pairs(Bubble.Games) do
     end
 end
 
+CoreGui.DescendantAdded:Connect(function(Ins)
+    if Ins.Name == "LeaveButton" then
+        task.delay(1, function()
+            TeleportService:Teleport(PlaceId, Players.LocalPlayer)
+        end)
+    end
+end)
+
 if not Bubble.Loaded then
     warn("Bubble does not support this game")
-    return
 end
